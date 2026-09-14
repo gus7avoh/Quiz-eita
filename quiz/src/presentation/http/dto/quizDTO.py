dc1d1e446300bd85b3b759c1670a372590578c9c
Gustavo Henrique Alves Maciel <gustavoh.a.m1409@gmail.com>
2026-09-13 22:33:29 -0300

    #05 - new Develop:  criando DTO para comunicacao api

--- quiz/src/presentation/http/dto/quizDTO.py
+++ quiz/src/presentation/http/dto/quizDTO.py
@@ -1,4 +1,5 @@
 from src.domain.entities.quiz import Quiz
+import random
 
 
 class QuizDTO:
@@ -6,21 +7,43 @@ class QuizDTO:
     def __init__(self, quiz: Quiz):
         self.quiz = quiz
 
-    def sort_questions(self):
-        pass
-    
+    def sort_alternatives(self, alternatives: list):
+        random.shuffle(alternatives)
+        return alternatives
 
-    def get_question(self, enunciado: str):
-        pass
+    def get_question(self):
+        return [
+            {
+                "enunciado": question.enunciado,
+                "alternativas": self.sort_alternatives(
+                    question.alternativas.copy()
+                )
+            }
+            for question in self.quiz.perguntas
+        ]
 
-
-    def is_correct_answer(self, enunciado: str, resposta_usuario: str):
-        pass
-
+    def is_correct_answer(
+        self,
+        question,
+        resposta_usuario: str
+    ):
+        return question.resposta == resposta_usuario
 
     def get_answer(
         self,
         enunciado: str,
         resposta_usuario: str
     ):
-        pass
+        for question in self.quiz.perguntas:
+
+            if question.enunciado == enunciado:
+                return {
+                    "resposta": question.resposta,
+                    "correta": self.is_correct_answer(
+                        question,
+                        resposta_usuario
+                    ),
+                    "explicacao": question.explicacao
+                }
+
+        return None
